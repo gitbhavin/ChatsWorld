734b81b34619f1b8f481883c90067f167f414ab0
gitbhavin <bhavin14patel@gmail.com>
2021-01-08 17:13:42 -0500

    Attached Property Complete

---
+++ ChatsWorld/AttachedProperties/BaseAttachedProperty.cs
@@ -0,0 +1,100 @@
+﻿
+using System;
+using System.Windows;
+
+namespace ChatsWorld
+{
+    /// <summary>
+    /// A base attached property to replace vanilla/convenstional wpf attached property 
+    /// </summary>
+    /// <typeparam name="Parent">the parent class to be the attched property</typeparam>
+    /// <typeparam name="Property">type of the attached property</typeparam>
+    public abstract class BaseAttachedProperty<Parent, Property>
+        where Parent : BaseAttachedProperty<Parent, Property>, new()
+
+    {
+       
+
+       
+        #region Public Properties
+
+        /// <summary>
+        /// A singleton instance of the parent class
+        /// </summary>
+        public static Parent Instance { get; private set; } = new Parent();
+
+        #endregion
+
+        #region Attached Property Defination
+
+        /// <summary>
+        /// Attached Property for this class
+        /// </summary>
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.RegisterAttached("Value", typeof(Property), typeof(BaseAttachedProperty<Parent, Property>), new PropertyMetadata(new PropertyChangedCallback(OnValuePropertyChanged)));
+
+        /// <summary>
+        /// the Callback event when the <see cref="ValueProperty"/> is changed
+        /// </summary>
+        /// <param name="d"> The UI element that had it's property changed</param>
+        /// <param name="e">The arguments for the event</param>
+        private static void OnValuePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            //call the parent function
+            Instance.OnValueChanged(d, e);
+
+            //call event listener
+             Instance.ValueChanged(d, e);
+        }
+
+        /// <summary>
+        /// Gets the attched property
+        /// </summary>
+        /// <param name="d">The element to get the value from</param>
+        /// <returns></returns>
+        public static Property GetValue(DependencyObject d)
+        {
+            return ((Property)d.GetValue(ValueProperty));
+        }
+
+        // Above function cal also be written using Lambda expression
+        // public static Property GetValue(DependencyObject d) => (Property)d.GetValue(ValueProperty);
+
+
+        /// <summary>
+        /// Sets the attched Property
+        /// </summary>
+        /// <param name="d">The element to get the value from </param>
+        /// <param name="value">The Value to set to</param>
+        //public static void SetValue(DependencyObject d, Property value)
+        //{
+        //    d.SetValue(ValueProperty, value);
+        //}
+
+        //Above function can be written using Lambda Expression
+        public static void SetValue(DependencyObject d, Property value) => d.SetValue(ValueProperty, value);
+
+        #endregion
+
+        #region Public Events
+
+        /// <summary>
+        /// Fired when the value changes
+        /// </summary>
+        public event Action<DependencyObject, DependencyPropertyChangedEventArgs> ValueChanged = (sender, e) => { };
+        #endregion
+
+
+        #region Event Methods
+
+        /// <summary>
+        /// The Method which is called when any property of this type Changed
+        /// </summary>
+        /// <param name="Sender">The Ui elemenmt that this property was changed for </param>
+        /// <param name="e">the arguments for this event</param>
+        public virtual void OnValueChanged(DependencyObject Sender, DependencyPropertyChangedEventArgs e) { }
+        #endregion
+    }
+
+
+
+}
---
+++ ChatsWorld/AttachedProperties/PasswordBoxAttachedProperty.cs
@@ -0,0 +1,63 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ChatsWorld
+{
+
+
+    /// <summary>
+    /// The MonitorPassword attched property for <see cref="PasswordBox"/>
+    /// </summary>
+    public class MonitorPasswordProperty : BaseAttachedProperty<MonitorPasswordProperty, bool>
+    {
+        public override void OnValueChanged(DependencyObject Sender, DependencyPropertyChangedEventArgs e)
+        {
+            //Get the caller
+            var passwordBox = Sender as PasswordBox;
+
+            //Make sure it is password Box
+            if (passwordBox == null)
+                return;
+
+            // Remove any previous events
+            passwordBox.PasswordChanged -= PasswordBox_PasswordChanged;
+
+            //If the caller set MonitorPassword to true...
+            if ((bool)e.NewValue)
+            {
+                //set the default value
+                HasTextProperty.SetValue(passwordBox);
+
+                //Start listning out for password changes
+                passwordBox.PasswordChanged += PasswordBox_PasswordChanged;
+            }
+        }
+
+        /// <summary>
+        /// Fired when the Password Box password changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            //Set the attached HasText value
+            HasTextProperty.SetValue((PasswordBox)sender);
+        }
+    }
+
+    /// <summary>
+    /// The HasText attched property for <see cref="PasswordBox"/>
+    /// </summary>
+    public class HasTextProperty : BaseAttachedProperty<HasTextProperty, bool>
+    {
+        /// <summary>
+        /// Sets the HasText propert based on if the caller <see cref="PasswordBox"/> has any text
+        /// </summary>
+        /// <param name="sender"></param>
+        public static void SetValue(DependencyObject sender)
+        {
+            SetValue(sender, ((PasswordBox)sender).SecurePassword.Length > 0);
+        }
+    }
+}
